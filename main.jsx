977b1db03e15d252de4a69d3422d4bacd97d7dc8
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-04-27 13:19:49 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GHCAcademy from './ghc-academy-final.jsx';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GHCAcademy />
+  </React.StrictMode>
+);
