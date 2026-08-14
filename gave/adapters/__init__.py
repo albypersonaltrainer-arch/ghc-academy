cebce75f1e3cd05d49f40e19e9055a7f3b982c60
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-08-14 12:30:02 +0200

    chore(gave): export online Wan adapter

--- gave/adapters/__init__.py
+++ gave/adapters/__init__.py
@@ -1,3 +1,9 @@
 from .comfyui_wan22_ti2v import ComfyUIWan22TI2VAdapter, GaveSafetyError
+from .hf_gradio_wan22_ti2v import HFGradioWan22TI2VAdapter, ZeroCostGuardError
 
-__all__ = ["ComfyUIWan22TI2VAdapter", "GaveSafetyError"]
+__all__ = [
+    "ComfyUIWan22TI2VAdapter",
+    "GaveSafetyError",
+    "HFGradioWan22TI2VAdapter",
+    "ZeroCostGuardError",
+]
